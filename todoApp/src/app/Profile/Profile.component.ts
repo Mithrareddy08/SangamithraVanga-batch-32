ce473afa83ee69fc2f0e627a0fd86e7a9bf72603
Mithrareddy08 <sangamithravanga@gmail.com>
2023-06-26 15:31:09 -0500

    new assignment added

---
+++ todoApp/src/app/Profile/Profile.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnChanges, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ProfileService } from './Profile.service';
+@Component({
+  selector: 'app-profile',
+  templateUrl: './Profile.component.html',
+  styleUrls: ['./Profile.component.css']
+})
+export class ProfileComponent implements OnInit, OnChanges, OnDestroy {
+  // life cycle hooks.
+  profileInfo: any;
+  constructor(private router: Router,
+    private pService: ProfileService) { 
+    // console.log('constructor');
+    
+  }
+  ngOnChanges() {
+    // console.log('Test in onchanges');
+  }
+ 
+  ngOnInit() { 
+    // console.log('Test in ngOnInit');
+    this.profileInfo = this.pService.getProfile();
+    
+  }
+   ngOnDestroy() {
+    // console.log('Test in Destroty');
+   }
+  
+  onAccountselectedInfo(event: any) {
+    console.log('event', event);
+    alert(event.balance);
+  }
+
+}
